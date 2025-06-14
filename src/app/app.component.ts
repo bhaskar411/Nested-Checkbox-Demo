1effa112592b152bfb1a65c559e0a7e9fcab594d
Bhaskar Kumar <113969676+bhaskar411@users.noreply.github.com>
2025-06-14 15:28:17 +0000

    Checkbox rendered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'CodeSandbox';
+export class AppComponent implements OnInit {
+  title = 'Nested Checkbox';
+  checkedItems = new Set<number>([1, 2, 5]);
+  checkboxData = [
+    {
+      id: 1,
+      name: 'Fruits',
+      children: [
+        {
+          id: 2,
+          name: 'Banana',
+          children: [
+            { id: 10, name: 'Cavendish' },
+            { id: 11, name: 'Lady Finger' },
+          ],
+        },
+        { id: 3, name: 'Apple' },
+        { id: 4, name: 'Mango' },
+        { id: 5, name: 'Strawberry' },
+      ],
+    },
+    {
+      id: 6,
+      name: 'Vegetables',
+      children: [
+        { id: 7, name: 'Carrot' },
+        {
+          id: 8,
+          name: 'Broccoli',
+          children: [
+            { id: 12, name: 'Calabrese' },
+            { id: 13, name: 'Sprouting' },
+          ],
+        },
+      ],
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  onToggle(id: number): void {}
 }
